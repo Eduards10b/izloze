fa60208e99d9296cd6e42964c707a9662e63c398
Eduards10b <eduards.kasparovics@gmail.com>
2022-03-08 11:14:38 +0200

    StundasDarbs

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ const vardi=['Normunds Eglītis', 'Elizabete Koklētāja',' Diāna Mostova',' Ed
 const balvas=['Čipši', 'Telefons', 'Planšetdators',' Dators', 'Dāvanu karte'];
 const naudaKopa=3500;
 let uzvaretajuSkaits=5;
+let rindas = document.querySelector('.rindas');//pievieno mainīgo
+let uzvaretajs=vardi
 
 for(let i=0;i<uzvaretajuSkaits;i++){
 
 let rand=Math.random()*vardi.length;
 rand=Math.floor(rand);  //noapalo uz leju
 let uzvaretajs = vardi[rand];// izvada konsolē
-console.log(vardi[rand]);
+let uzvaretajs=vardi[rand];
 }
+rindas.innerHTML+=
+<tr>
+<td>${i+1}</td>
 
 
 
