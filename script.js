52799b977ba137a779214feb55d515abfc72ecdb
Eduards10b <eduards.kasparovics@gmail.com>
2022-03-08 13:14:35 +0200

    Otrdiena

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const vardi=['Normunds Eglītis', 'Elizabete Koklētāja',' Diāna Mostova',' Edgars Kuranovs',' Renārs Valdems'];
-const balvas=['Čipši', 'Telefons', 'Planšetdators',' Dators', 'Dāvanu karte'];
-const naudaKopa=3500;
-let uzvaretajuSkaits=5;
+const vardi = ['Normunds Eglītis', 'Elizabete Koklētāja', ' Diāna Mostova', ' Edgars Kuranovs', ' Renārs Valdems'];
+const balvas = ['Čipši', 'Telefons', 'Planšetdators', ' Dators', 'Dāvanu karte'];
+const naudaKopa = 3500;
+let uzvaretajuSkaits = 5;
 let rindas = document.querySelector('.rindas');//pievieno mainīgo
-let uzvaretajs=vardi
+rindas.innerHTML = '';
 
-for(let i=0;i<uzvaretajuSkaits;i++){
+for (let i = 0; i < uzvaretajuSkaits; i++) {
 
-let rand=Math.random()*vardi.length;
-rand=Math.floor(rand);  //noapalo uz leju
-let uzvaretajs = vardi[rand];// izvada konsolē
-let uzvaretajs=vardi[rand];
-}
-rindas.innerHTML+=
+    let rand = Math.random() * vardi.length;
+    rand = Math.floor(rand);  //noapalo uz leju
+    let uzvaretajs = vardi[rand];// izvada konsolē
+    //let uzvaretajs=vardi[rand];
+    rindas.innerHTML += `
+
+
 <tr>
-<td>${i+1}</td>
-
+<td>${i + 1}</td>
+<td>${uzvaretajs}</td>
+</tr>`
+}
 
 
 
