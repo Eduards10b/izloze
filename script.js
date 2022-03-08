8521271082651b96f1594ec2d98b18c6c2e29041
Eduards10b <eduards.kasparovics@gmail.com>
2022-03-08 10:57:11 +0200

    Pirmais

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const vrdi=['Normunds Eglītis', 'Elizabete Koklētāja',' Diāna Mostova',' Edgars Kuranovs',' Renārs Valdems'];
+const vardi=['Normunds Eglītis', 'Elizabete Koklētāja',' Diāna Mostova',' Edgars Kuranovs',' Renārs Valdems'];
 const balvas=['Čipši', 'Telefons', 'Planšetdators',' Dators', 'Dāvanu karte'];
-const naudaKopa= 2000;
-let uzvaretajuSkaits= 5;
-Math.random=Math.random()*10;
+const naudaKopa=3500;
+let uzvaretajuSkaits=5;
 
+for(let i=0;i<uzvaretajuSkaits;i++){
 
-
-
-
-
-
-
+let rand=Math.random()*vardi.length;
+rand=Math.floor(rand);  //noapalo uz leju
+let uzvaretajs = vardi[rand];// izvada konsolē
 console.log(vardi[rand]);
+}
+
+
+
+
