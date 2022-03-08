ddac82567ec74b77a1fc8890e6ce624a9efe5e8f
Eduards10b <eduards.kasparovics@gmail.com>
2022-03-08 10:18:23 +0200

    Pirmais

---
+++ script.js
@@ -0,0 +1,14 @@
+const vrdi=['Normunds Eglītis', 'Elizabete Koklētāja',' Diāna Mostova',' Edgars Kuranovs',' Renārs Valdems'];
+const balvas=['Čipši', 'Telefons', 'Planšetdators',' Dators', 'Dāvanu karte'];
+const naudaKopa= 2000;
+let uzvaretajuSkaits= 5;
+Math.random=Math.random()*10;
+
+
+
+
+
+
+
+
+console.log(vardi[rand]);
