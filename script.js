77f6e73567445ca50a6277505f8771bda9f4d622
Eduards10b <eduards.kasparovics@gmail.com>
2022-03-22 11:15:55 +0200

    Pirmais

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const vardi = ['Normunds Eglītis', 'Elizabete Koklētāja', ' Diāna Mostova', ' Edgars Kuranovs', ' Renārs Valdems'];
 const balvas = ['Čipši', 'Telefons', 'Planšetdators', ' Dators', 'Dāvanu karte'];
-const naudaKopa = 3500;
+const naudaKopa = 10000;
 let uzvaretajuSkaits = 5;
-let rindas = document.querySelector('.rindas');//pievieno mainīgo
-rindas.innerHTML = '';
+let rindas=document.querySelector('.rindas');//pievieno mainīgo
+rindas.innerHTML = ' ';
 
-for (let i = 0; i < uzvaretajuSkaits; i++) {
+for (let i=0;i<uzvaretajuSkaits;i++) {
 
-    let rand = Math.random() * vardi.length;
-    rand = Math.floor(rand);  //noapalo uz leju
-    let uzvaretajs = vardi[rand];// izvada konsolē
+    let rand=Math.random()*vardi.length;
+    rand=Math.floor(rand);  //noapalo uz leju
+    let uzvaretajs=vardi[rand];// izvada konsolē
+    let laimests = balvas[rand];
     //let uzvaretajs=vardi[rand];
-    rindas.innerHTML += `
-
-
+   
+    rindas.innerHTML +=`
 <tr>
-<td>${i + 1}</td>
+<td>${i+1}</td>
 <td>${uzvaretajs}</td>
+<td>${laimests}</td>
 </tr>`
 }
 
